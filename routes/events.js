973988048d13cd99a37aaca3308a838b1b08d663
moebiusfaktor <alexmoebius13@gmail.com>
2019-07-08 21:53:33 +0200

    continue with the profile.js - need to create form and populate with the user info

--- routes/events.js
+++ routes/events.js
@@ -3,32 +3,39 @@ const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 const Event = require("../models/Event");
-// const Event = require("../models/Event");
 
 router.get("/createevent", (req, res, next) => {
-  res.render("events/createevent", { message: req.flash("error") });
+ res.render("events/createevent", { message: req.flash("error") });
 });
 
 router.post("/createevent", (req, res, next) => {
-  const { name, description, latitude, longtitude } = req.body;
+ const { name, description, imageUrl, latitude, longtitude } = req.body;
+ Event.create({
+   name,
+   imageUrl,
+   description,
+   location: {
+     type: "Point",
+     coordinates: [latitude, longtitude]
+   }
+ })
+   .then(data => {
+     res.redirect("/");
+   })
+   .catch(err => {
+     console.log(err);
+   });
+});
 
-  console.log(name, description, latitude, longtitude);
-
-  // Event.create({
-  //   name,
-  //   imageUrl,
-  //   location: {
-  //     type: "Point",
-  //     coordinates: [latitude, longitude]
-  //   }
-  // })
-
-  // .then(data => {
-  //   res.redirect("/");
-  // })
-  // .catch(err => {
-  //   console.log(err);
-  // });
+router.get("/events", (req, res, next) => {
+ Event.find({})
+   .then(events => {
+     console.log(events);
+     res.render("events/events", { eventsList: events });
+   })
+   .catch(err => {
+     console.log("Error while retrieving the books: ", err);
+   });
 });
 
 module.exports = router;
