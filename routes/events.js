d29cd9780487fbfe7ad8b964783a2fddf89732fb
Alican Torun <torun.alican@gmail.com>
2019-07-12 12:57:30 +0200

    nearly done

--- routes/events.js
+++ routes/events.js
@@ -36,7 +36,9 @@ router.post("/createevent", (req, res, next) => {
   } = req.body;
   Event.create({
     name,
-    imageUrl,
+    imageUrl:
+      imageUrl ||
+      "https://images.unsplash.com/photo-1509803874385-db7c23652552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
     description,
     keyCat,
     date,
